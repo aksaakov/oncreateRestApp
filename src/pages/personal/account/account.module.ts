7f5e76e30ed2ae5affc1aa1b39d5046547dff0ec
thefigtreestudio <info@thefigtreestudio.com>
2018-12-12 13:53:42 +0000

    First Commit on this branch

---
+++ src/pages/personal/account/account.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AccountPage } from './account';
+import { TranslateModule } from '@ngx-translate/core';
+
+@NgModule({
+	declarations: [
+		AccountPage
+	],
+	imports: [
+		IonicPageModule.forChild(AccountPage),
+		TranslateModule.forChild()
+	]
+})
+export class AccountPageModule {}
