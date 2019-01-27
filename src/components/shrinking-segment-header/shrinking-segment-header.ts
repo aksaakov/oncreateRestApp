103bdbcc1e6934063391354e92a9a2f2105975f8
aksaakov <a.cealicov@gmail.com>
2019-01-27 11:36:08 +0000

    added parallax header (components)

---
+++ src/components/shrinking-segment-header/shrinking-segment-header.ts
@@ -0,0 +1,45 @@
+import { Component, Input, ElementRef, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'shrinking-segment-header',
+  templateUrl: 'shrinking-segment-header.html'
+})
+export class ShrinkingSegmentHeader {
+
+  @Input('scrollArea') scrollArea: any;
+  @Input('headerHeight') headerHeight: number;
+
+  newHeaderHeight: any;
+  scrollcontent: any;
+  constructor(public element: ElementRef, public renderer: Renderer2) {
+
+  }
+
+  ngAfterViewInit(){
+
+    this.renderer.setStyle(this.element.nativeElement, 'height', this.headerHeight + 'px');
+
+    this.scrollArea.ionScroll.subscribe((ev) => {
+      this.resizeHeader(ev);
+    });
+
+  }
+
+  resizeHeader(ev){
+
+    ev.domWrite(() => {
+
+      this.newHeaderHeight = this.headerHeight - ev.scrollTop;
+
+      if(this.newHeaderHeight < 0){
+        this.newHeaderHeight = 0;
+      }
+
+      this.renderer.setStyle(this.element.nativeElement, 'height', this.newHeaderHeight + 'px');
+
+
+    });
+
+  }
+
+}
